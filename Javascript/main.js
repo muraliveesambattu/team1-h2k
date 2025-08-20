301dc3426c73bba7fe478240d4abe697ed579bf0
vmurali100 <vmurali100@gmail.com>
2025-08-20 08:33:08 +0530

    Adding Objects Intro

---
+++ Javascript/main.js
@@ -0,0 +1,20 @@
+// Data Types In JS 
+// Srings 
+// NUmbers 
+// Boolen
+// null
+// Undeifned
+
+
+var myName = "Murali ";
+var myMobile = 9663856625 ;
+var areFullstack = false;
+var currentProject = null ;
+var featureProject ; // undefined 
+
+// Output 
+// console.log("Hello JS "); // this will Show the Output In Debugging windo 
+// console.log(fullName)
+// alert("Welcome to JS")
+// document.writeln("Hello World ")
+// document.getElementById("demo").innerHTML = "Hello from JS with Document Method !!!"
